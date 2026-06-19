02edcb9a342cc27b02d6b8c590cec2c403556577
SOKLEAP <phorkzokl3ap@gmail.com>
2026-06-19 14:07:28 +0700

    fix api route

--- routes/api.php
+++ routes/api.php
@@ -47,74 +47,125 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::prefix('categories')->group(function (){
-        Route::get('/', [CategoriesController::class, 'index']);
+        // Route::get('/', [CategoriesController::class, 'index']);
         Route::post('/', [CategoriesController::class, 'store']);
-        Route::get('/{id}', [CategoriesController::class, 'show']);
+        // Route::get('/{id}', [CategoriesController::class, 'show']);
         Route::put('/{id}', [CategoriesController::class, 'update']);
         Route::delete('/{id}', [CategoriesController::class, 'destroy']);
     });
 
     Route::prefix('brands')->group(function (){
-        Route::get('/', [BrandsController::class, 'index']);
+        // Route::get('/', [BrandsController::class, 'index']);
         Route::post('/', [BrandsController::class, 'store']);
-        Route::get('/{id}', [BrandsController::class, 'show']);
+        // Route::get('/{id}', [BrandsController::class, 'show']);
         Route::put('/{id}', [BrandsController::class, 'update']);
         Route::delete('/{id}', [BrandsController::class, 'destroy']);
     });
     
     Route::prefix('products')->group(function (){
-        Route::get('/', [ProductsController::class, 'index']);
+        // Route::get('/', [ProductsController::class, 'index']);
         Route::post('/', [ProductsController::class, 'store']);
-        Route::get('/{id}', [ProductsController::class, 'show']);
+        // Route::get('/{id}', [ProductsController::class, 'show']);
         Route::put('/{id}', [ProductsController::class, 'update']);
         Route::delete('/{id}', [ProductsController::class, 'destroy']);
     });
     
     Route::prefix('orders')->group(function (){
-        Route::get('/', [OrdersController::class, 'index']);
+        // Route::get('/', [OrdersController::class, 'index']);
         Route::post('/', [OrdersController::class, 'store']);
-        Route::get('/{id}', [OrdersController::class, 'show']);
+        // Route::get('/{id}', [OrdersController::class, 'show']);
         Route::put('/{id}', [OrdersController::class, 'update']);
         Route::delete('/{id}', [OrdersController::class, 'destroy']);
     });
 
     Route::prefix('order-items')->group(function (){
-        Route::get('/', [OrderItemsController::class, 'index']);
+        // Route::get('/', [OrderItemsController::class, 'index']);
         Route::post('/', [OrderItemsController::class, 'store']);
-        Route::get('/{id}', [OrderItemsController::class, 'show']);
+        // Route::get('/{id}', [OrderItemsController::class, 'show']);
         Route::put('/{id}', [OrderItemsController::class, 'update']);
         Route::delete('/{id}', [OrderItemsController::class, 'destroy']);
     });
 
     Route::prefix('payments')->group(function (){
-        Route::get('/', [PaymentsController::class, 'index']);
+        // Route::get('/', [PaymentsController::class, 'index']);
         Route::post('/', [PaymentsController::class, 'store']);
-        Route::get('/{id}', [PaymentsController::class, 'show']);
+        // Route::get('/{id}', [PaymentsController::class, 'show']);
         Route::put('/{id}', [PaymentsController::class, 'update']);
         Route::delete('/{id}', [PaymentsController::class, 'destroy']);
     });
 
     Route::prefix('invoices')->group(function (){
-        Route::get('/', [InvoicesController::class, 'index']);
+        // Route::get('/', [InvoicesController::class, 'index']);
         Route::post('/', [InvoicesController::class, 'store']);
-        Route::get('/{id}', [InvoicesController::class, 'show']);
+        // Route::get('/{id}', [InvoicesController::class, 'show']);
         Route::put('/{id}', [InvoicesController::class, 'update']);
         Route::delete('/{id}', [InvoicesController::class, 'destroy']);
     });
 
-    Route::prefix('favourites')->group(function (){
-        Route::get('/', [FavouritesController::class, 'index']);
-        Route::post('/', [FavouritesController::class, 'store']);
-        Route::get('/{id}', [FavouritesController::class, 'show']);
-        Route::put('/{id}', [FavouritesController::class, 'update']);
-        Route::delete('/{id}', [FavouritesController::class, 'destroy']);
-    });
+    // Route::prefix('favourites')->group(function (){
+    //     Route::get('/', [FavouritesController::class, 'index']);
+    //     Route::post('/', [FavouritesController::class, 'store']);
+    //     Route::get('/{id}', [FavouritesController::class, 'show']);
+    //     Route::put('/{id}', [FavouritesController::class, 'update']);
+    //     Route::delete('/{id}', [FavouritesController::class, 'destroy']);
+    // });
 
-    Route::prefix('cards')->group(function (){
-        Route::get('/', [CardsController::class, 'index']);
-        Route::post('/', [CardsController::class, 'store']);
-        Route::get('/{id}', [CardsController::class, 'show']);
-        Route::put('/{id}', [CardsController::class, 'update']);
-        Route::delete('/{id}', [CardsController::class, 'destroy']);
-    });
+    // Route::prefix('cards')->group(function (){
+    //     Route::get('/', [CardsController::class, 'index']);
+    //     Route::post('/', [CardsController::class, 'store']);
+    //     Route::get('/{id}', [CardsController::class, 'show']);
+    //     Route::put('/{id}', [CardsController::class, 'update']);
+    //     Route::delete('/{id}', [CardsController::class, 'destroy']);
+    // });
+});
+
+Route::prefix('categories')->group(function (){
+    Route::get('/', [CategoriesController::class, 'index']);
+    Route::get('/{id}', [CategoriesController::class, 'show']);
+});
+
+Route::prefix('brands')->group(function (){
+    Route::get('/', [BrandsController::class, 'index']);
+    Route::get('/{id}', [BrandsController::class, 'show']);
+});
+
+Route::prefix('products')->group(function (){
+    Route::get('/', [ProductsController::class, 'index']);
+    Route::get('/{id}', [ProductsController::class, 'show']);
+});
+
+Route::prefix('orders')->group(function (){
+    Route::get('/', [OrdersController::class, 'index']);
+    Route::get('/{id}', [OrdersController::class, 'show']);
+});
+
+Route::prefix('order-items')->group(function (){
+    Route::get('/', [OrderItemsController::class, 'index']);
+    Route::get('/{id}', [OrderItemsController::class, 'show']);
+});
+
+Route::prefix('payments')->group(function (){
+    Route::get('/', [PaymentsController::class, 'index']);
+    Route::get('/{id}', [PaymentsController::class, 'show']);
+});
+
+Route::prefix('invoices')->group(function (){
+    Route::get('/', [InvoicesController::class, 'index']);
+    Route::get('/{id}', [InvoicesController::class, 'show']);
+});
+
+Route::prefix('favourites')->group(function (){
+    Route::get('/', [FavouritesController::class, 'index']);
+    Route::post('/', [FavouritesController::class, 'store']);
+    Route::get('/{id}', [FavouritesController::class, 'show']);
+    Route::put('/{id}', [FavouritesController::class, 'update']);
+    Route::delete('/{id}', [FavouritesController::class, 'destroy']);
+});
+
+Route::prefix('cards')->group(function (){
+    Route::get('/', [CardsController::class, 'index']);
+    Route::post('/', [CardsController::class, 'store']);
+    Route::get('/{id}', [CardsController::class, 'show']);
+    Route::put('/{id}', [CardsController::class, 'update']);
+    Route::delete('/{id}', [CardsController::class, 'destroy']);
 });
