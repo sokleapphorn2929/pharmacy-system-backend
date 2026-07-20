a2890052ece49f1fd9086626063e95eb61d19ac6
SOKLEAP <phornzokl3ap@gmail.com>
2026-07-20 16:30:36 +0700

    back delivery test

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -38,7 +38,7 @@ class PaymentsController extends Controller
             'payment_status' => 'required|in:unpaid,paid,refunded',
         ]);
 
-        $validatedData['total_price'] = floatval($request->total_price) + 2;
+        // $validatedData['total_price'] = floatval($request->total_price) + 2;
 
         $validatedData['user_id'] = auth()->id();
 
