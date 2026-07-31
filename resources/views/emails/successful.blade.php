dae096f228c57abe199e1e6634749fb222f6c888
SOKLEAP <phornzokl3ap@gmail.com>
2026-07-31 23:35:26 +0700

    fix logo size and add delivery fee

--- resources/views/emails/successful.blade.php
+++ resources/views/emails/successful.blade.php
@@ -10,7 +10,7 @@
 Thank you for your purchase. Your payment has been successfully processed.
 
 **Order ID:** {{ $orderData['_id'] ?? $orderData['id'] }}  
-**Total Amount:** ${{ $orderData['total_price'+1] ?? 'N/A' }}
+**Total Amount:** ${{ isset($orderData['total_price']) ? number_format($orderData['total_price'] + 1, 2) : 'N/A' }}
 
 You can log in to your dashboard anytime to check your ongoing order status and notifications.
 
