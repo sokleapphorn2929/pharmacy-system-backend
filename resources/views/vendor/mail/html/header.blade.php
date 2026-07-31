260ea8a44b7f55018ee02f376c96e7ed171490b6
SOKLEAP <phornzokl3ap@gmail.com>
2026-07-31 23:40:04 +0700

    fix link in logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
+<a href="https://pharmacy-frontend-taupe.vercel.app/" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://res.cloudinary.com/ds1e6ptad/image/upload/v1785514196/pharmacy_logo_fpb0hs.png" class="logo" alt="Kh Pharmacy Logo">
 @else
